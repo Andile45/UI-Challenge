64419ce925e10d32bb11cf1a4c6a3ca49d0b56bd
Andile45 <manganyeandile@gmail.com>
2025-09-07 15:56:45 +0200

    i removed inline styling so that i can add media queries

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,30 +3,26 @@ import Logo from '../assets/logo.svg'
 import FBIcon from '../assets/icon-facebook.svg'
 import TwitterIcon from '../assets/icon-twitter.svg'
 import InstagramIcon from '../assets/icon-instagram.svg'
-import '../components/footer.css'
+import './footer.css'
 
 export const Footer = () => {
   return (
-    <div className='footer' style={{display:'flex', flexDirection:'row', justifyContent:'space-between', marginTop:'4rem' , backgroundColor:'lightgray', marginInline:'0', padding:'50px'}}>
+    <div className='footer'>
+      <img src={Logo} alt="Logo" className="logoImg" />
 
-        <img src={Logo} alt="Logo" className="logoImg" />
+      <ol className="footerLinks">
+        <li>FAQs</li>
+        <li>Contact Us</li>
+        <li>Privacy Policy</li>
+        <li>Press Kit</li>
+        <li>Install Guide</li>
+      </ol>
 
-        <ol style={{listStyle:'none' , display:'flex',flexDirection:'row', gap:'1.5rem'}}>
-            <li>FAQs</li>
-            <li>Contact Us</li>
-            <li>Privacy Policy</li> <br />
-            <li>Press Kit</li>
-            <li>Install Guide</li>
-        </ol>
-
-        <div className="socialMediaIcons" style={{display:'flex' , flexDirection:'row', gap:'10px'}}>
-            <img src={FBIcon} alt="" className="facebook"  style={{maxHeight:40}}/>
-
-            <img src={TwitterIcon} alt="" className="twitter"  style={{maxHeight:40}}/>
-
-            <img src={InstagramIcon} alt="" className="instagram" style={{maxHeight:40}}/>
-        </div>
-
+      <div className="socialMediaIcons">
+        <img src={FBIcon} alt="Facebook" className="facebook" />
+        <img src={TwitterIcon} alt="Twitter" className="twitter" />
+        <img src={InstagramIcon} alt="Instagram" className="instagram" />
+      </div>
     </div>
   )
 }
